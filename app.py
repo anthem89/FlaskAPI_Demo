7faa3a6ffd3b5bf9cd4d5e06d265940cd6988b70
Chris Yu <christopher.yu89@gmail.com>
2021-09-13 22:27:10 -0400

    minor commit

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ def test():
     ret = json.dumps([res.address, res.neighborhoods_gdf.to_json()])
     return ret
 
+    # A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to geospatial-api</h1>"
+
 if __name__ == '__main__':
     # # To Run on local host
     # app.run(host='localhost', port=3000)
