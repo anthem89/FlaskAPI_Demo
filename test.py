cd3008a5aa23afc19b6c50932821d8ccbf0a2180
Chris Yu <christopher.yu89@gmail.com>
2021-09-13 22:53:17 -0400

    commented out suspect code to try to get heroku to buld correctly

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
-import geopandas as gpd
-from neighborhood import *
+# import geopandas as gpd
+# from neighborhood import *
 
-def ReturnGeoSpatialData(address, distance, distanceUnit):
-    #location of the shape file
-    infile_shp='./Shape_Files/Boston_Neighborhoods-shp'
-    #geopandas read shape file and convert to geodataframe
-    shp_gdf = gpd.read_file(infile_shp).to_crs(epsg=4326)
+# def ReturnGeoSpatialData(address, distance, distanceUnit):
+#     #location of the shape file
+#     infile_shp='./Shape_Files/Boston_Neighborhoods-shp'
+#     #geopandas read shape file and convert to geodataframe
+#     shp_gdf = gpd.read_file(infile_shp).to_crs(epsg=4326)
 
-    #instantiate the class
-    n = neighborhood_of_a_point(address, shp_gdf)
+#     #instantiate the class
+#     n = neighborhood_of_a_point(address, shp_gdf)
 
-    #returns coordinates of the address
-    n.coordinates_gdf.geometry
+#     #returns coordinates of the address
+#     n.coordinates_gdf.geometry
 
-    #returns geometry of the shape
-    n.loc_gdf.geometry
+#     #returns geometry of the shape
+#     n.loc_gdf.geometry
 
-    n.set_buffer(distance, distanceUnit)
-    #returns geometry of the radius around the address
-    n.buffer_gdf.geometry
+#     n.set_buffer(distance, distanceUnit)
+#     #returns geometry of the radius around the address
+#     n.buffer_gdf.geometry
 
-    #returns geometry of the shapes which intersect the radius around the address
-    n.set_neighborhoods()
-    n.neighborhoods_gdf.geometry
+#     #returns geometry of the shapes which intersect the radius around the address
+#     n.set_neighborhoods()
+#     n.neighborhoods_gdf.geometry
 
-    return n
+#     return n
 
-if __name__ == '__main__':
-    # ReturnGeoSpatialData('150 Brookline Avenue, Boston, MA', 1, "miles")
-    pass
+# if __name__ == '__main__':
+#     # ReturnGeoSpatialData('150 Brookline Avenue, Boston, MA', 1, "miles")
+#     pass
